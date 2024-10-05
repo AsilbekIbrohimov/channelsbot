1bc15d50b86a609cf0054997c79cd8b116946d7e
asilbekpy <158860866+asilbekpy@users.noreply.github.com>
2024-10-06 01:45:07 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,8 +1,8 @@
-from environs import Env
+# from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = "6792619983:AAGmllbnG2U5NpAHlOuLdZloIpQalR6xz1o"#env.str("BOT_TOKEN")  # Bot toekn
