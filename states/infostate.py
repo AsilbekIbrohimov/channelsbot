3eef3acdad99288fc8b96b466f1249e873063c32
asilbekpy <158860866+asilbekpy@users.noreply.github.com>
2024-10-06 00:54:35 +0500

    uploaded

---
+++ states/infostate.py
@@ -0,0 +1,13 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+class info(StatesGroup):
+    channel_name = State()
+    username = State()
+    admin = State()
+    language = State()
+    sub = State()
+    price = State()
+    period = State()
+    more = State()
+    logo = State()
+    ha = State()
