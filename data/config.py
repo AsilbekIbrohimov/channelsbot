2357bfeecf220e6e715bb17bee518bb537b2724e
asilbekpy <158860866+asilbekpy@users.noreply.github.com>
2024-10-08 20:15:44 +0500

    bot token

--- data/config.py
+++ data/config.py
@@ -5,6 +5,6 @@
 # env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = "6792619983:AAGmllbnG2U5NpAHlOuLdZloIpQalR6xz1o"#env.str("BOT_TOKEN")  # Bot toekn
+BOT_TOKEN = "7877072755:AAGZcv2SWZqzRmZu2NgDefaw0SQljDsnJuA"#env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = [1024522810,1680816082]#env.list("ADMINS")  # adminlar ro'yxati
 IP = "localhost"#env.str("ip")  # Xosting ip manzili
